48554e19126f3c231f1b9feb158a0d56ec2b4869
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-04-24 04:33:24 +0700

    feat: update HomePresenter to render monos from MonoMockModel

--- src/presenters/HomePresenter.js
+++ src/presenters/HomePresenter.js
@@ -1,7 +1,8 @@
 import HomeView from '../views/HomeView'
+import getMonos from '../models/MonoMockModel'
 
 export default class HomePresenter {
   init() {
-    HomeView.render()
+    HomeView.render(getMonos())
   }
 }
