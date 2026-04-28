cd355e5e4aa8bbfc7c13e78ce40b431bbb4f914c
faridreaming <97384053+faridreaming@users.noreply.github.com>
2026-04-28 14:57:45 +0700

    feat: update MonoView to enhance empty state messaging and improve list rendering

--- src/views/MonoView.js
+++ src/views/MonoView.js
@@ -6,9 +6,9 @@ export default class MonoView {
         <input id="mono-drawer" type="checkbox" class="drawer-toggle" checked />
         <div class="drawer-content h-full">
           <section class="h-full min-h-0 p-4 flex flex-col gap-4">
-            <div id="mono-filter" class="border border-base-300 bg-base-100 p-3">
+            <!-- <div id="mono-filter" class="border border-base-300 bg-base-100 p-3">
               <button class="btn btn-ghost btn-sm">Filter</button>
-            </div>
+            </div> -->
 
             <div id="mono-map" class="flex-1 min-h-0 border border-base-300 bg-base-200"></div>
           </section>
@@ -58,5 +58,39 @@ export default class MonoView {
 
   static renderList(monos = []) {
     const monoListSidebarEl = document.getElementById('mono-list-sidebar')
+    monoListSidebarEl.innerHTML =
+      monos.length === 0
+        ? `
+          <div role="alert" class="alert m-4 p-4 alert-vertical">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="lucide lucide-info-icon lucide-info h-4.5 w-4.5" aria-hidden="true">
+              <circle cx="12" cy="12" r="10" />
+              <path d="M12 16v-4" />
+              <path d="M12 8h.01" />
+            </svg>
+            <span>Belum ada <em class="font-bold">mono</em>. Tambahkan <em class="font-bold">mono</em> pertamamu.</span>
+          </div>
+        `
+        : `
+          ${monos.map(
+            (mono) => `
+              <div class="card card-side bg-base-100 shadow-sm">
+                <figure class="h-20 w-20 shrink-0">
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    alt="Movie" />
+                </figure>
+                <div class="card-body">
+                  <h2 class="card-title">New movie is released!</h2>
+                  <p>Click the button to watch on Jetflix app.</p>
+                  <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Watch</button>
+                  </div>
+                </div>
+              </div>
+            `,
+          )}
+        `
   }
 }
